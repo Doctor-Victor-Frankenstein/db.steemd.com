acabdcb7c7a9c9c4260a464ca86ae4da347bbd7a
Tim <roadscape@users.noreply.github.com>
2016-10-10 22:36:10 -0400

    unicorn and gitignore

---
+++ config/unicorn.rb
@@ -0,0 +1,17 @@
+@dir = File.expand_path("./../../", __FILE__) + "/"
+
+worker_processes 8
+working_directory @dir
+
+timeout 1800
+
+# Specify path to socket unicorn listens to,
+# we will use this in our nginx.conf later
+listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+
+# Set process id path
+pid "#{@dir}tmp/pids/unicorn.pid"
+
+# Set log file paths
+stderr_path "#{@dir}log/unicorn.stderr.log"
+stdout_path "#{@dir}log/unicorn.stdout.log"
